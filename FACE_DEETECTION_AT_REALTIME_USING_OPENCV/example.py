96555bc89acc93ff891e81ea5d372c3a202c4371
SRINATH N S S <srinath.cs20@bitsathy.ac.in>
2023-02-03 22:12:18 +0530

    Initial commit

---
+++ FACE_DEETECTION_AT_REALTIME_USING_OPENCV/example.py
@@ -0,0 +1,29 @@
+import cv2
+
+# Enable WebCam
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 420)
+
+#importing necessary Functions
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml")
+
+
+while True:
+    success, img = cap.read()
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(imgGray, 1.3, 5)  
+  
+    for (x, y, w, h) in faces:
+        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    eyes = eyeCascade.detectMultiScale(imgGray)
+    for (ex, ey, ew, eh) in eyes:
+        img = cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 3)
+
+    cv2.imshow('face_detect', img)
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyWindow('face_detect')
